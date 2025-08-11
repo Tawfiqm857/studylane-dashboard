80def8228c7f25113379f95dbc995a3cf46b3645
devByTawfiq <ahmadtawfiqm857@gmail.com>
2025-08-11 20:27:25 +0100

    updated HashRouter syntax error

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ const App = () => (
     <TooltipProvider>
       <AuthProvider>
         <TestProvider>
-          <Router>
+          <HashRouter>
             <div className="min-h-screen flex flex-col">
               <Header />
               <main className="flex-1">
@@ -55,7 +55,7 @@ const App = () => (
             </div>
             <Toaster />
             <Sonner />
-          </Router>
+          </HashRouter>
         </TestProvider>
       </AuthProvider>
     </TooltipProvider>
