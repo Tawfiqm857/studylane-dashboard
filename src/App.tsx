a11220dcadde430ebcbcbe27c7b543917a7fa420
AhmadTtawfiq Muhammad <144332302+Tawfiqm857@users.noreply.github.com>
2025-08-11 21:22:15 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { HashRouter as Router , Routes, Route} from "react-router-dom";
+import { BrowserRouter as Router , Routes, Route} from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext";
 import { TestProvider } from "./contexts/TestContext";
 import Header from "./components/Header";
@@ -24,7 +24,7 @@ const App = () => (
     <TooltipProvider>
       <AuthProvider>
         <TestProvider>
-          <Router>
+          <BrowserRouter>
             <div className="min-h-screen flex flex-col">
               <Header />
               <main className="flex-1">
@@ -55,7 +55,7 @@ const App = () => (
             </div>
             <Toaster />
             <Sonner />
-          </Router>
+          </BrowserRouter>
         </TestProvider>
       </AuthProvider>
     </TooltipProvider>
